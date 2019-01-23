f87dc8210b533fae1745d5f38a55e307148aef60
Emmanuel Bravo <eebravo1@gmail.com>
2019-01-23 13:11:47 -0600

    completed initial graphql server setup

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,25 @@
-const mutations = {};
+const Mutations = {
+	createItem: async function(parent, args, ctx, info) {
+		//TODO: check if they are logged in
+		const item = await ctx.db.mutation.createItem(
+			{
+				data: {
+					...args
+				}
+			},
+			info
+		);
+		return item;
+	}
 
-module.exports = mutations;
+	// createDog: function(parent, args, ctx, info) {
+	// 	global.dogs = global.dogs || [];
+	// 	//create a dog!
+	// 	const newDog = { name: args.name };
+	// 	global.dogs.push(newDog);
+	// 	return newDog;
+	// 	// console.log(args);
+	// }
+};
+
+module.exports = Mutations;
